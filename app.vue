7d115a577bb64d2330eadbdffb2e47ac9af4c0dc
Alexander <sansanichick@gmail.com>
2022-06-05 14:34:26 +1000

    init

---
+++ app.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+    import CustomButton from './components/CustomButton.vue';
+
+    const val = ref(0);
+    
+    const increment = () => {
+        val.value++;
+    }
+
+    const bar = (e: Event) => {
+        console.log(e);
+    }
+
+    const arr = [
+        {
+            id: 0,
+            text: "Pidor"
+        },
+        {
+            id: 1,
+            text: "Rukav"
+        },
+        {
+            id: 2,
+            text: "Timosh"
+        }
+    ]
+</script>
+
+<template>
+    <div>
+        <input type="text" v-bind:value="val">
+        <button v-on:click="increment">
+            click
+        </button>
+        <!-- <CustomButton v-if="val === 2">kek</CustomButton>
+        <div v-else-if="val === 3">Rukav pidor</div>
+        <div v-else>Timosh dai labu</div> -->
+
+        <div>
+            <div v-for="el in arr" :key="el.id">
+                <CustomButton>
+                    <h4>{{ el.id }}</h4>
+                    <p>{{ el.text }}</p>
+                </CustomButton>
+            </div>
+        </div>
+
+        <NuxtPage />
+    </div>
+</template>
+
+<style>
+    div {
+        display: grid;
+    }
+</style>
---
+++ components/CustomButton.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+    // const props = defineProps<{
+    //     label: string
+    // }>();
+
+    const emit = defineEmits<{
+        (e: 'fun', ev: Event): void
+    }>();
+
+    const foo = (e: Event) => {
+        emit('fun', e);
+    }
+
+    onMounted(() => {
+        console.log("Mounted")
+    });
+</script>
+
+<template>
+    <div>
+        <button class="my-button" @click="foo">
+            <slot></slot>
+        </button>
+    </div>
+</template>
+
+<style scoped>
+    button {
+        padding: 40px;
+        margin: 10px;
+        user-select: none;
+        background-color: aqua;
+        color: blue;
+    }
+</style>
